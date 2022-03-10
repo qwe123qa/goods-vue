5193952cc0f0ce1821fb3387469c1fa379522ac3
qwe123qa <ts01173166@gmail.com>
2022-03-10 18:21:04 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    "no-mixed-spaces-and-tabs": 0,
+    "no-unused-vars": "off",
+    "no-undef": 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
